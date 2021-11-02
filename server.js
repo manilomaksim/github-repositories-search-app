2fb5e8e30d22951436fdfe5c579e3a6a8275ea53
Maksim <ManiloM.dev@gmail.com>
2021-11-02 17:54:20 +0300

    added settings for heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/github-repositories-search-app'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/github-repositories-search-app/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiGitHubUrl: 'https://api.github.com/search/repositories'
 };
